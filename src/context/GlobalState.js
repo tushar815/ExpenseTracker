7b776b7af04dac59dc06d5c215ce6424bc1b6efd
Tushar Kalbhor <=tskalbhor@gmail.com>
2024-06-20 22:20:41 -0600

    add Context

---
+++ src/context/GlobalState.js
@@ -0,0 +1,27 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from './AppReducer'
+
+//intial state
+const initialState = {
+  transactions: [
+    { id: 1, text: "Flower", amount: -20 },
+    { id: 2, text: "Salary", amount: 300 },
+    { id: 3, text: "Book", amount: -10 },
+    { id: 4, text: "Camera", amount: 150 },
+  ],
+};
+
+//Create Context
+export const GlobalContext = createContext(initialState);
+
+//Create Provider
+//dispatch function lets you update the state to a different value and trigger a rerender.
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
